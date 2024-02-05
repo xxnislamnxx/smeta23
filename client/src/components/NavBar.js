f5b99dccc6ec127faff052a33738e183f247053b
xxnislamnxx <darian9888@mail.ru>
2024-02-05 21:28:03 +0300

    a

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min.js'
-import { ADMIN_ROUTE, LOGIN_ROUTE, PROJECT_ROUTE, STATISTICS_ROUTE, USERLIST_ROUTE } from '../utils/consts.js'
+import { PROJECT_ROUTE,MATERIALS_ROUTE,TYPESOFWORK_ROUTE,PREMISES_ROUTE,STAGES_ROUTE } from '../utils/consts.js'
 import {Button} from 'react-bootstrap'
 import {observer} from 'mobx-react-lite'
 import {useHistory} from "react-router-dom/cjs/react-router-dom";
@@ -16,93 +16,46 @@ import { getOneOtdel } from '../http/otdelAPI.js'
     const {user} = useContext(Context)
     const {otdel} = useContext(Context)
     const history = useHistory()
-    const tok = localStorage.getItem('token')  
-    let token
+  //   const tok = localStorage.getItem('token')  
+  //   let token
 
-    useEffect(() => {
-      if (!user.isAuth) {
-        token = user.token[0]
-        console.log("не авторизован",token.Name)
-        otdel.setOneOtdel([{Name:''}])
-     }else{
-       token = jwtDecode(tok)
-       console.log("авторизован",token.Name)
-       getOneOtdel(token.Otdel_id).then(data => otdel.setOneOtdel(data))
-     }
+  //   useEffect(() => {
+  //     if (!user.isAuth) {
+  //       token = user.token[0]
+  //       console.log("не авторизован",token.Name)
+  //       otdel.setOneOtdel([{Name:''}])
+  //    }else{
+  //      token = jwtDecode(tok)
+  //      console.log("авторизован",token.Name)
+  //      getOneOtdel(token.Otdel_id).then(data => otdel.setOneOtdel(data))
+  //    }
       
-  }, [])
+  // }, [])
 
     
     /*const token = jwtDecode(localStorage.getItem('token'))
     
     */
   
-    const logOut =() => {
-      user.setUser({})
-      user.setIsAuth(false)
-      localStorage.removeItem("token")
-      history.push(LOGIN_ROUTE)
-    }
+    // const logOut =() => {
+    //   user.setUser({})
+    //   user.setIsAuth(false)
+    //   localStorage.removeItem("token")
+    //   history.push(LOGIN_ROUTE)
+    // }
     return (
       
       <Navbar bg="dark" data-bs-theme="dark">
         <Container >
-        <Navbar.Brand href={USERLIST_ROUTE}>Задачник</Navbar.Brand>
-          {user.isAuth ?
+        <Navbar.Brand href="#home">Смета</Navbar.Brand>
             <Nav className="mr-2" style={{color:'white'}}>
-              <Nav.Link>Мои проекты</Nav.Link>
-              <Nav.Link>Материалы</Nav.Link>
-              <Nav.Link>Виды работ</Nav.Link>
-              <Nav.Link>Помещения</Nav.Link>
-              <Nav.Link>Этапы</Nav.Link>
-              {/* <label 
-                className="d-flex align-items-center"
-                style={{marginRight: "10px"}}
-                >
-                  {(jwtDecode(localStorage.getItem('token')).Name)} | {otdel.OneOtdel.Name}
-                </label>
-              <Button 
-                variant={"outline-light"} 
-                style={{marginLeft: "20px"}}
-                onClick={() => history.push(STATISTICS_ROUTE)}>Мои проекты
-              </Button>
-
-              <Button 
-                variant={"outline-light"} 
-                style={{marginLeft: "20px"}}
-                onClick={() => history.push(PROJECT_ROUTE)}>Материалы
-              </Button>
-
-              <Button 
-                variant={"outline-light"} 
-                style={{marginLeft: "20px"}}
-                onClick={() => history.push(PROJECT_ROUTE)}>Виды работ
-              </Button>
-
-              <Button 
-                variant={"outline-light"} 
-                style={{marginLeft: "20px"}}
-                onClick={() => history.push(PROJECT_ROUTE)}>Помещения
-              </Button>
-
-              <Button 
-                variant={"outline-light"} 
-                style={{marginLeft: "20px"}}
-                onClick={() => history.push(PROJECT_ROUTE)}>Этапы
-              </Button>
-
-              <Button 
-                variant={"outline-light"} 
-                style={{marginLeft: "20px"}}
-                onClick={() => logOut()}>Выйти
-              </Button> */}
-                
+              <Nav.Link href={PROJECT_ROUTE}>Мои проекты</Nav.Link>
+              <Nav.Link href={MATERIALS_ROUTE}>Материалы</Nav.Link>
+              <Nav.Link href={TYPESOFWORK_ROUTE}>Виды работ</Nav.Link>
+              <Nav.Link href={PREMISES_ROUTE}>Помещения</Nav.Link>
+              <Nav.Link href={STAGES_ROUTE}>Этапы</Nav.Link>
             </Nav>
-            :
-            <Nav className="ml-auto" style={{color:'white'}}>
-              <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</Button>
-            </Nav>
-          }
+            
         </Container>
     </Navbar>
     )
